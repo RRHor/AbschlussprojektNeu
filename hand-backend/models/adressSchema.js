e5f36e71a79159fff79b964a480abf353b6b6dc9
rrhor <r.r.horn@t-online.de>
2025-06-18 10:21:54 +0200

    chore: comment out address schema and model definitions in adressSchema.js

--- hand-backend/models/adressSchema.js
+++ hand-backend/models/adressSchema.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const adressSchema = new mongoose.Schema({
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  district: { type: String, required: true },
-  zip: { type: Number, required: true }
-});
+// const adressSchema = new mongoose.Schema({
+//   street: { type: String, required: true },
+//   city: { type: String, required: true },
+//   district: { type: String, required: true },
+//   zip: { type: Number, required: true }
+// });
 
-const Adress = mongoose.models.Adress || mongoose.model('Adress', adressSchema);
-export default Adress;
+// const Adress = mongoose.models.Adress || mongoose.model('Adress', adressSchema);
+// export default Adress;
